06cdcde00138e93ea9de8ed9a104ed9f9b0217d7
syevaazmi <145574147+syevaazmi@users.noreply.github.com>
2024-05-07 16:38:52 +0700

    Add files via upload

--- soal_3.py
+++ soal_3.py
@@ -1,12 +1,7 @@
-def main():
-    total_items = int(input("Masukkan Jumlah Barang: "))
-    total_price = 0
+jumlah_barang = int(input("Masukkan jumlah barang: "))
+total_harga = 0
+for i in range(jumlah_barang):
+        harga_barang = float(input("Masukkan harga barang ke-{}: ".format(i + 1)))
+        total_harga += harga_barang
 
-    for i in range(1, total_items + 1):
-        price = float(input(f"Masukkan Harga Barang ke-{i}: "))
-        total_price += price
-
-    print("Total Belanjaan:", total_price)
-
-if __name__ == "__main__":
-    main()
+print("Total harga belanja:", total_harga)
